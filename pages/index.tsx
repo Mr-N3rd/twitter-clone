07a0f6de11625a86e5cd66c969b957b627f66add
Leif Sandvar <n3rdvana@outlook.com>
2023-08-08 02:18:00 -0700

    Added create user, and logon function, workaround is ugly for auth features, leaving project variable empty

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 
 import { Client, Databases, Account } from 'appwrite';
-
+import { useEffect } from 'react'
 const endpoint_var= process.env.API_ENDPOINT
 const project_var = process.env.API_PROJECT
   const client = new Client
@@ -10,23 +10,50 @@ const project_var = process.env.API_PROJECT
 
 export default function Home(tweets: any) {
   console.log(tweets)
-
+  useEffect(() => {
+    
+  },[]  )
 
 
   const createUser = async () => {
-      const client = new Client
-  client.setEndpoint(`https://api.luckynerd.studio/v1`)
-    .setProject(project_var)
+    const client = new Client
+    client
+      .setEndpoint(`https://api.luckynerd.studio/v1`)
+      .setProject(``)
     const account = new Account(client)
-    const response = await account.create(
+    
+    const response = account.create(
       'username',
-      'email',
+      'email@email.com',
       'password'
-    )
-    console.log(response)
+    );
+
+    response.then(function (response) {
+      console.log(response);
+    }, function (error) {
+      console.log(error);
+    })
 
   }
-  
+    const logonUser = async () => {
+    const client = new Client
+    client
+      .setEndpoint(`https://api.luckynerd.studio/v1`)
+      .setProject(``)
+    const account = new Account(client)
+    
+    const response = account.createEmailSession(
+      'email@email.com',
+      'password'
+    );
+
+    response.then(function (response) {
+      console.log(response);
+    }, function (error) {
+      console.log(error);
+    })
+
+  }
   return (
     <>
       <div className="container">
@@ -39,10 +66,21 @@ export default function Home(tweets: any) {
             <input type="text" placeholder="Username" className="px-3 mx-3 input input-bordered input-primary w-full max-w-xs" />
             <input type="text" placeholder="Password" className="input mx-3 input-bordered input-primary w-full max-w-xs" />
             <input type="text" placeholder="Email" className="input mx-3 input-bordered input-primary w-full max-w-xs" />
-
           </form>
         </div>
-                    <button onClick={createUser}>Submit</button>
+        <div className={`my-3`}>
+          <label>Create User</label>
+            <span className={`border-r-blue-950 py-3 rounded border-2 py-1 px-3 mx-2` }>
+            <button onClick={createUser}>Submit</button>
+            </span>
+        </div>
+        <div className={`my-6`}>
+          <label>logon User</label>
+            <span className={`border-r-blue-950 py-3 rounded border-2 py-1 px-3 mx-2` }>
+            <button onClick={logonUser}>Submit</button>
+        </span>
+        </div>
+
       </div>
     </>
   )
